51a4966f68a2383a88c48c28d016f762440458db
floriansr <sueurfloriansf@gmail.com>
2020-10-06 16:41:52 +0200

    [add] styletron provider client

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 
+import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
+import { Client as Styletron } from 'styletron-engine-atomic';
+
 const App = () => {
+  const debug =
+    process.env.NODE_ENV === 'production' ? undefined : new DebugEngine();
+
+  const engine = new Styletron();
   return (
     <>
-      <h3>Hello world !</h3>
+      <StyletronProvider value={engine} debug={debug} debugAfterHydration>
+        <h3>Hello world !</h3>
+      </StyletronProvider>
     </>
   );
 };
